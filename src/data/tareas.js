3c7bb91c2601a078fc28a78ada81abc15a7548e7
AndreaKass <aksierra@unicesar.edu.co>
2026-04-25 20:10:22 -0500

    feat: añadir datos

---
+++ src/data/tareas.js
@@ -0,0 +1,12 @@
+export const tareasIniciales = [
+  { id: 1, titulo: "Parcial de Cálculo", materia: "Matemáticas",
+    fecha: "2025-05-10", completada: false },
+  { id: 2, titulo: "Informe de Laboratorio", materia: "Física",
+    fecha: "2025-05-08", completada: true },
+  {id: 3, titulo: "Taller de Programación Web", materia: "Programación Web",
+    fecha: "2026-03-25", completada: true },
+  {id: 4, titulo: "Exposición de Software", materia: "Ingeniería de Software",
+    fecha: "2026-02-10", completada: false },
+  {id: 5, titulo: "Parcial de Cátedra Económica", materia: "Cátedra Económica",
+    fecha: "2026-04-01", completada: true },
+];
